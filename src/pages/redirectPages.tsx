185765735b6528821dc0455d1c2b1b1388ed6288
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2025-05-11 04:07:11 -0400

    fix: muestra error si no encuentra enlace corto

--- src/pages/redirectPages.tsx
+++ src/pages/redirectPages.tsx
@@ -1,22 +1,45 @@
 import Loading from "@/components/loading";
 import { getOriginalLink } from "@/services/firebase";
+import { Container, Text, VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 function RedirectPages(){
   const { shortId } = useParams();
+  const [link, setlink] = useState(true);
   
   const getLink = async (shortId:string | undefined) => {
     if(shortId === undefined) return;
     getOriginalLink(shortId).then((res:{linkOriginal:string|null})=>{
-      console.log(res);
       if(res.linkOriginal){
         window.location.href = res.linkOriginal;
+      }else{
+        setlink(false);
       }
     });
   }
 
-  getLink(shortId);
+  useEffect(() => {
+    getLink(shortId);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  return <Loading />
+  if(link) return <Loading />
+  return (
+    <Container maxW="100vw" h="100vh" p={0} bg="gray.50" centerContent>
+      <VStack
+        gap={8}
+        w={{ base: "90%", md: "450px" }}
+        justify="center"
+        h="100%"
+      >
+        <VStack gap={2} mb={4} alignItems="center" >
+          <Text color="gray.500" textAlign="center" fontSize="xl">
+            Link not found
+          </Text>          
+        </VStack>
+      </VStack>
+    </Container>
+  )
 }
 export default RedirectPages;
