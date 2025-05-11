5f9296273773cfe7ce5c0c5825b35091fa2337a4
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2025-05-11 03:18:49 -0400

    algunos cambios en vite config para reducir los archivos compilados

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,37 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import inspect from 'vite-plugin-inspect'
 import compression from 'vite-plugin-compression'
+import inspect from 'vite-plugin-inspect'
 import { resolve } from 'path'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
   plugins: [
     react({
       tsDecorators: true,
-      jsxImportSource: "@emotion/react",
-      // plugins: [["@swc/plugin-styled-components", {}]],
+      jsxImportSource: '@emotion/react',
     }),
-    inspect(),
-    compression({ algorithm: 'gzip' }), // gzip
-    compression({ algorithm: 'brotliCompress' }), // brotli
-  ],
+    isProd &&
+      compression({
+        algorithm: 'gzip',
+        ext: '.gz',
+        deleteOriginFile: false,
+      }),
+    isProd &&
+      compression({
+        algorithm: 'brotliCompress',
+        ext: '.br',
+        deleteOriginFile: false,
+      }),
+    !isProd && inspect(),
+  ].filter(Boolean),
   cacheDir: 'node_modules/.vite_cache',
   optimizeDeps: {
     esbuildOptions: {
       target: 'esnext',
-      supported: {
-        'dynamic-import': true,
-      },
+      supported: { 'dynamic-import': true },
     },
     exclude: ['lucide-react'],
   },
@@ -49,9 +58,23 @@ export default defineConfig({
     target: 'esnext',
     cssTarget: 'chrome100',
     minify: 'esbuild',
-    sourcemap: true,
+    sourcemap: false,
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
+    reportCompressedSize: true,
+    manifest: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
+  define: {
+    'process.env': {}, // Prevents accidental leaks from system env vars
   },
 })
