483dddffb63f056368cf29544c3105970c9a6d94
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2025-06-04 06:38:01 -0400

    feat: agrega nuevas condiciones a la contraseña

--- src/schema/schemas.ts
+++ src/schema/schemas.ts
@@ -7,11 +7,23 @@ export const loginSchema = z.object({
 
 export const registerSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(8).max(30),
-  confirm: z.string().min(8).max(30),
+  password: z.string().min(8, { message: "Password must be at least 8 characters" }),
+  confirm: z.string().min(8),
 }).refine((data) => data.password === data.confirm, {
   message: "Passwords don't match",
   path: ["confirm"],
+}).refine((data) => /[a-z]/.test(data.password), {
+  message: "Password must contain at least one lowercase letter",
+  path: ["password"],
+}).refine((data) => /[A-Z]/.test(data.password), {
+  message: "Password must contain at least one uppercase letter",
+  path: ["password"],
+}).refine((data) => /[0-9]/.test(data.password), {
+  message: "Password must contain at least one number",
+  path: ["password"],
+}).refine((data) => /[!¡@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(data.password), {
+  message: "Password must contain at least one special character",
+  path: ["password"],
 });
 
 export const searchSchema = z.object({
